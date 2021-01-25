c5646943eb44ba0396c687892072fdf4d6ed608b
dongchul <sorkehdcjf@gmail.com>
2021-01-25 23:32:54 +0900

    feat: Job Scope 와 Batch Job Instance

--- src/main/java/me/study/springbatch/job/SimpleJobConfiguration.java
+++ src/main/java/me/study/springbatch/job/SimpleJobConfiguration.java
@@ -1,12 +1,15 @@
 package me.study.springbatch.job;
 
 import lombok.RequiredArgsConstructor;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,15 +23,25 @@ public class SimpleJobConfiguration {
     @Bean
     public Job simpleJob() {
         return jobBuilderFactory.get("SimpleJob")
-                .start(simpleStep1())
+                .start(simpleStep1(null))
                 .build();
     }
 
+    //
+    /**
+     * @JobScope 선언 후, jobParameters를 받는다.
+     * 실행 파라미터로 requestDate=20210125 와 같이 전달. 동일한 값인 경우 Job Instance 가 생성되지 않는다.
+     *  A job instance already exists and is complete for parameters={requestDate=20210125}.  If you want to run this job again, change the parameters.
+     * 
+    * */
+
     @Bean
-    public Step simpleStep1() {
+    @JobScope
+    public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
         return stepBuilderFactory.get("simpleStep1")
                 .tasklet((contribution, chunkContext) -> {
                     log.info(">>>>> This is Step1");
+                    log.info(">>>>> requestDate = {}", requestDate);
                     return RepeatStatus.FINISHED;
                 })
                 .build();
