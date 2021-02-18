b1fc9e9fb282ddee52139aaca9e6af0ceb72643f
dongchul <sorkehdcjf@gmail.com>
2021-02-18 18:47:29 +0900

    feat: Spring Batch 트랜잭션 범위 확인

---
+++ src/main/java/me/study/springbatch/domain/ClassInformation.java
@@ -0,0 +1,12 @@
+package me.study.springbatch.domain;
+
+public class ClassInformation {
+    private String teacherNAme;
+
+    private long studentsCount;
+
+    public ClassInformation(String teacherNAme, long studentsCount) {
+        this.teacherNAme = teacherNAme;
+        this.studentsCount = studentsCount;
+    }
+}
---
+++ src/main/java/me/study/springbatch/job/TransactionProcessorJobConfiguration.java
@@ -0,0 +1,79 @@
+package me.study.springbatch.job;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import me.study.springbatch.domain.ClassInformation;
+import me.study.springbatch.domain.Teacher;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManagerFactory;
+
+/**
+ * Spring Batch에서의 트랜잭션 범위는 Chunk 단위이다.
+ * 예제 코드의 processor 에서 LazyLoading이 잘 동작하는 것을 확인할 수 있다.
+ *
+ */
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class TransactionProcessorJobConfiguration {
+
+    public static final String JOB_NAME = "transactionProcessorBatch";
+    public static final String BEAN_PREFIX = JOB_NAME + "_";
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final EntityManagerFactory entityManagerFactory;
+
+    @Value("${chunkSize:1000}")
+    private int chunkSize;
+
+    @Bean(JOB_NAME)
+    public Job job() {
+        return jobBuilderFactory.get(JOB_NAME)
+                .preventRestart()
+                .start(step())
+                .build();
+    }
+
+    @Bean(BEAN_PREFIX + "step")
+    @JobScope
+    public Step step() {
+        return stepBuilderFactory.get(BEAN_PREFIX + "step")
+                .<Teacher, ClassInformation>chunk(chunkSize)
+                .reader(reader())
+                .processor(processor())
+                .writer(writer())
+                .build();
+    }
+
+    @Bean
+    public JpaPagingItemReader<Teacher> reader() {
+        return new JpaPagingItemReaderBuilder<Teacher>()
+                .name(BEAN_PREFIX + "reader")
+                .entityManagerFactory(entityManagerFactory)
+                .pageSize(chunkSize)
+                .queryString("select t from Teacher t")
+                .build();
+    }
+
+    @Bean
+    public ItemProcessor<Teacher, ClassInformation> processor() {
+        return teacher -> new ClassInformation(teacher.getName(), teacher.getStudents().size());
+    }
+
+    private ItemWriter<ClassInformation> writer() {
+        return System.out::println;
+    }
+}
